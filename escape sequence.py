b18e6872199ef70b21b1648525fc062e803576d1
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-05 22:54:58 +0545

    extended

--- escape sequence.py
+++ escape sequence.py
@@ -14,3 +14,27 @@ print("anmol \t valu")
 print("anmol \t valu")
 print("ashif \t valu")
 print("ashif \t valu")
+
+#print("\")= it gives error
+# 
+print("\\")
+""" it gives one \ backslash """
+print("\\\\")
+""" it will give two backslach """
+
+
+
+
+# print following
+""" these is \\ double backslash """
+print("these is \\\\ double backslach")
+
+
+""" /\ /\ /\ these are the mountain """
+print("/\\ /\\ /\\ these are the mountain")
+
+""" he is   awesome """
+print("he is \t awesome")
+
+""" \"  \n \t \'  """
+print(" \\"" \\n \\t\\' ")
