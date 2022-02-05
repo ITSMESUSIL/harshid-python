76a9a09c2e7d6b1602e73361fa5c192d32705665
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-05 18:20:24 +0545

    harshid python

---
+++ escape sequence.py
@@ -0,0 +1,16 @@
+# 1 
+print("hi iam fine"and " you")
+# 2
+print(' hello "dear"')
+
+#3
+print("hey whats up \n dear")
+print(' one \n two')
+print("i want second\n paragraph")
+
+# \t
+print("sushil \t kathayat")
+print("anmol \t valu")
+print("anmol \t valu")
+print("ashif \t valu")
+print("ashif \t valu")
