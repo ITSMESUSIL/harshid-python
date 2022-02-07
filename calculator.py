0c385fdce14fa325e60f2d21ef874656016ca607
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-07 20:59:23 +0545

    calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+""" 2**4 it means to power to the no."""
+print (3**3) 
+print(4**4)
+
+#% it gives the remainder
+print(10%4)
+print(11%5)
+
+# // it gives the output before the decimal
+print(10//6)
+print(100//9)
+
+# / it give float value
+print(5/2)
+print(9/3)
+
+#exponent
+print(3**2**2)
+""" first = 3**2
+second = 3**6"""
