45f491f43c392ab579e0fe29f57a19e3495cd21b
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-10 02:05:20 +0545

    initial:end:step

---
+++ string slicing step.py
@@ -0,0 +1,5 @@
+name = "sushil"
+print(name[::2])# syntax[initial:endpoint:step]  it means print the string step ahead
+print(name[4:6])
+print(name[5::-1])# -1 will print letter step down by 1
+print(name[-1::-1])
