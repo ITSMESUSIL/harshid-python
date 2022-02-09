672ec124f54a77c4bafcc248e4ded942f95d714e
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-10 02:18:47 +0545

    step in string slicing

--- string slicing step.py
+++ string slicing step.py
@@ -3,3 +3,9 @@ print(name[::2])# syntax[initial:endpoint:step]  it means print the string step 
 print(name[4:6])
 print(name[5::-1])# -1 will print letter step down by 1
 print(name[-1::-1])
+
+
+#excercise
+name=input("enter your name ",)
+print(f"your reverse is {name[-1::-1]}")
+print("your reverse is {}".format(name[-1::-1]))
