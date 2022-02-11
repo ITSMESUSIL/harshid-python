b86499de74fe0c5a9a89c0680b1bcbaab2bbd9f0
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-11 18:19:42 +0545

    replace and find in string

---
+++ replace and find.py
@@ -0,0 +1,14 @@
+string= "my self sushil kathayat"
+print(string.replace("my self","my name"))
+str2= "he is two years old and he is so cute"
+print(str2.replace("is","was"))
+print(str2.replace("is","was",1))
+print(str2.replace('is','was',2))
+
+
+#find()
+str3= "she is so beutifull and is very cute"
+print(str3.find("is"))
+print(str3.find("is",0))
+print(str3.find("is",4))
+print(str3.find("is",5))
