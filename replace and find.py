3781fd5ec1aff664f45181d884bc0e4f8f047c73
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-11 18:22:48 +0545

    replace and find in string

--- replace and find.py
+++ replace and find.py
@@ -2,8 +2,8 @@ string= "my self sushil kathayat"
 print(string.replace("my self","my name"))
 str2= "he is two years old and he is so cute"
 print(str2.replace("is","was"))
-print(str2.replace("is","was",1))
-print(str2.replace('is','was',2))
+print(str2.replace("is","was",1))# it will replace only one is
+print(str2.replace('is','was',2))# it will replace two is
 
 
 #find()
@@ -11,4 +11,4 @@ str3= "she is so beutifull and is very cute"
 print(str3.find("is"))
 print(str3.find("is",0))
 print(str3.find("is",4))
-print(str3.find("is",5))
+print(str3.find("is",5))# it will count after count 5
