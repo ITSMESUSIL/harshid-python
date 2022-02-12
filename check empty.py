484565ac551e97c21929c30f3ccdad4a06a723fd
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-12 21:02:37 +0545

    check empty

---
+++ check empty.py
@@ -0,0 +1,21 @@
+name= "sushil"
+if name: # non empty string symbolise true while empty resemble false
+    print("is not empty")
+else:
+    print("is empty")
+    
+    
+#but 
+name2= ""
+if name2:
+    print("the string is not empty")
+else:
+    print("the string is empty")
+    
+    
+#
+name3 = input("enter any name")
+if name3:
+    print(f"your name is {name3}")
+else:
+    print(f"you didnt enter any name")
