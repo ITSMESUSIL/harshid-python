ea957331b2262591e552718c05c2c93a66c58ae7
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-13 23:34:27 +0545

    string method in forloop

---
+++ string methodFORLOOP.py
@@ -0,0 +1,18 @@
+name= "sushil"
+for i in name:
+    print(i)
+    """ 
+    s
+    u
+    s
+    h
+    i
+    l
+    """
+
+#sum the digits of taken number
+number= input("enter your number")
+sum1= 0
+for i in number:
+    sum1+=int(i)
+print(sum1)
