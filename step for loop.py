d1dc938fc3b8b9d7c181f4f82a7c596da9ad5c4f
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-13 23:20:15 +0545

    step in for loop range

---
+++ step for loop.py
@@ -0,0 +1,31 @@
+# 1
+""" for i in range(1,10):
+    print(i) # 1,2,3,4,5,6,7,8,9 """
+    
+    
+    
+# 2
+""" for i in range(1,10,2):
+    print(i)     #1,3,5,7,9 """
+    
+    
+    
+    
+# 3 to print even numbers
+""" for i in range(0,11,2):
+    if i==0:
+        continue
+    print(i)
+ """
+ 
+ 
+
+# to print 10 to 1
+for i in range(10,0,-1):
+    print(i)
+    
+    
+#2 to print 0 to -10
+for i in range(0,-11,-1):
+    print(i)
+    
