5e11379c200dbfa435336b04c4bdf9caffc004ab
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-20 19:16:50 +0545

    loop in list

---
+++ loop in list.py
@@ -0,0 +1,10 @@
+list= ["sushil","kathayat","mass"]
+''' for i in list:# it will tak all item in list
+    print(i)
+ '''
+
+# while loop in list
+''' x= 0
+while x<len(list):
+    print(list[x])
+    x=x+1 '''
