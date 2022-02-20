d45f94aa6c6b1dc2d48b9220431135f80a291e6a
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-20 18:36:01 +0545

    split in list

---
+++ split in list.py
@@ -0,0 +1,15 @@
+# to convert string into list
+''' string = "sushil 123,456".split()# it will break string after space and convert it into list
+print(string)
+ '''
+
+# 2
+''' type1="sushilkathayat".split("l")# it will break sting after l and make  list
+print(type1) '''
+
+
+# 3
+''' string="sushil kathayat stay in hotel".split()
+print(string)
+list= string.copy()
+print(list) '''
