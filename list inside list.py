eb87534bc19cc204c6df31e2c647e34725e190d0
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-22 17:14:27 +0545

    function use list

---
+++ list inside list.py
@@ -0,0 +1,19 @@
+''' list= [[1,2,3],[4,5,6],[8,9,10]]
+print(list[1][1])
+print(list[0][:])
+print(list[1][:])
+print(list[2][:])
+print(list)
+for num in list: # to print all numbers
+    for i in num:
+        print(i)
+
+ '''
+
+
+''' 
+list= [[["sushil","bottel"],["coke","kanxo"]],[["kathayat","hoss"],["pallo","kanxo"]],[["or","mar"],["more","and"]]]
+for list1 in list:
+    for list2 in list1:
+        for i in list2:
+            print(i) '''
