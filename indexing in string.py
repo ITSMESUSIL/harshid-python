ce2893fde00184a07a6a68455710f2ba48dcbcbb
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-22 14:58:29 +0545

    indexing in string

---
+++ indexing in string.py
@@ -0,0 +1,7 @@
+list= list(range(1,11))
+print(list)
+print(list.index(1))
+list.append(1)# i asign 1 in last in list so we can calculate index that is recently added
+print(list)
+
+print(list.index(1,1))# sytax= index(charcater,after which position)
