8efd0e5a5a27184f35ba38ce526082514267cac4
sushil kathayat <sushilkathayat10@gmail.com>
2022-02-22 19:00:55 +0545

    min and max in list

---
+++ min and max.py
@@ -0,0 +1,8 @@
+list= [1,2,4,10,100]
+print(min(list))# 1
+print(max(list))#100
+
+# excercise make function that take list and subtract low no. from high
+''' def subtract(l):
+    return max(l)-min(l)
+print(subtract(list)) '''
