64dcb6ca2d1492fad9c03ab81315be51bfbc9799
sushil kathayat <sushilkathayat10@gmail.com>
2022-03-08 01:23:39 +0545

    map function

--- map function.py
+++ map function.py
@@ -1,15 +1,18 @@
 # map
 # for example
 list1= [1,2,3,4,5]
-# print its squares
-list2=[]
+# method 1 print its squares
+''' list2=[]
 for i in list1:
     list2.append(i**2)
-print(list2)
+print(list2) '''
 
-# using map it will easier
+# method 2 using list comprehension
+''' list2=[i**2 for i in list1] '''
+
+# method 3 using map it will easier
 # actually you can use map instead of list comprehension
-def function1(a):
+''' def function1(a):
     return a**2
 numbers=tuple(map(function1,list1))
 print(numbers)
@@ -17,7 +20,7 @@ print(numbers)
 
 list4=["sushil","kathayat","last"]
 print(map(len,list4))
-
+ '''
 
 
 
